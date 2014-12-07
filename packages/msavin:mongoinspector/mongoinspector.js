1b203cd827ab0158bf7d26c819fbca6a48fbee85
Jon James <jon@threering.com>
2014-12-06 20:25:08 -0500

    use wrapped Meteor.Collection for tracking collections and slim down template code

--- packages/msavin:mongoinspector/mongoinspector.js
+++ packages/msavin:mongoinspector/mongoinspector.js
@@ -1,46 +1,28 @@
+var origCollection = Meteor.Collection,
+    activeCollections = [];
+
+Meteor.Collection = function (name, options) {
+    var instance = new origCollection(name, options);
+    activeCollections.push({
+        name: name,
+        instance: instance
+    });
+    return instance;
+};
+
 if (Meteor.isClient) {
 
     Template.body.helpers({
-        MongoInspector_enabled: function() {
-
-            var cordova   = Meteor.isCordova;
-
-
-            if (cordova   === true) {
-                return false;
-            } else {
-                return true;
-            }
-
-        },
         MongoInspector_collections: function () {
-              
-            // Detecting Collections automatically 
-            // Forked from shanedonnelly1 
+            return activeCollections;
 
-            var collections = [];
-
-            for (var globalObject in window) { 
-                if (globalObject === "webkitStorageInfo") {
-                    continue;
-                }
-                if (window[globalObject] instanceof Meteor.Collection) {
-                    collections.push({
-                        "name": globalObject,
-                        // "collection": window[globalObject]
-                    });
-                }
-            }
-
-            return collections;
-            
         }
     });
 
     Template.body.events({
         'click .MongoInspector_row': function () {
             var collectionName = this.name;
-            var thisCollection = getPropByString(window, collectionName);
+            var thisCollection = this.instance;
             console.log(thisCollection.find().fetch());
         },
         'click .MongoInspector_header': function () {
@@ -51,34 +33,9 @@ if (Meteor.isClient) {
     Template.MongoInspector_collection.helpers({
         collectionCount: function () {
             var collectionName = this.name;
-            var thisCollection = getPropByString(window, collectionName);
+            var thisCollection = this.instance;
             return thisCollection.find().count();
         }
     });
-
-    /* Thanks to Richard Smith (rjsmith)
-
-    /**
-     * @rsbatech: Recursively access sub-properties from a dotted property path
-     * From: http://stackoverflow.com/a/6906859
-     **/
-    function getPropByString(obj, propString) {
-        if (!propString)
-            return obj;
-
-        var prop, props = propString.split('.');
-
-        for (var i = 0, iLen = props.length - 1; i < iLen; i++) {
-            prop = props[i];
-
-            var candidate = obj[prop];
-            if (candidate !== undefined) {
-                obj = candidate;
-            } else {
-                break;
-            }
-        }
-        return obj[props[i]];
-    }
 
 }
